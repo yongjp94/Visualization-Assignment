f89e4ddbff1e9528e825e3c86a7190d2f63ed42e
yongjp94 <yongjun.park.94@gmail.com>
2016-04-13 17:00:10 -0700

    added two more visualizations

--- js/main.js
+++ js/main.js
@@ -11,22 +11,7 @@ $(document).ready(function() {
         header: true,
         dynamicTyping: true,
         complete: function(results, file) {
-            console.log("Parsing complete: ", results, file);
-            var resultData = formatData(results.data);
-            // alert(results.data[0]["Bacteria "]);
-            var layout = {
-                title: 'Effectiveness of Antibacterials',
-                barmode:'group',
-                xaxis: {
-                    title: 'Bacterias'
-                },
-                yaxis: {
-                    title: 'Minimum Inhibitory Concentration',
-                    type: 'log',
-                    autorange: true
-                }
-            };
-            Plotly.newPlot('graph-1', resultData, layout);
+            graphData(results,file);
         }
     }
     
@@ -35,6 +20,55 @@ $(document).ready(function() {
     // alert(result.data);
 });
 
+var graphData = function(results, file) {
+    console.log("Parsing complete: ", results, file);
+    
+    var resultData = formatData(results.data);
+    var resultData2 = formatData2(results.data);
+    
+    
+    var layout = {
+        title: 'Resilience of Bacterias to Antibacterials (Stacked)',
+        barmode:'group',
+        xaxis: {
+            title: 'Bacterias'
+        },
+        yaxis: {
+            title: 'Minimum Inhibitory Concentration',
+            type: 'log',
+            autorange: true
+        }
+    };
+    
+    var layout2 = {
+        title: 'Resilience of Bacterias to Antibacterials (Stacked)',
+        barmode:'stack',
+        xaxis: {
+            title: 'Bacterias'
+        },
+        yaxis: {
+            title: 'Minimum Inhibitory Concentration',
+            type: 'log',
+            autorange: true
+        }
+    };
+    
+    var layout3 = {
+        title: 'Resilience of Bacterias to Antibacterials (Box Plot)',
+        xaxis: {
+            title: 'Antibacterials'
+        },
+        yaxis: {
+            title: 'Minimum Inhibitory Concentration',
+            type: 'log',
+            autorange: true
+        }
+    };
+    
+    Plotly.newPlot('graph-1', resultData, layout);
+    Plotly.newPlot('graph-2', resultData, layout2);
+    Plotly.newPlot('graph-3', resultData2, layout3);
+};
 
 /*
  * Reads the parsed version of CSV file and formats them for Plotly
@@ -48,15 +82,16 @@ var formatData = function(antibacData) {
         peni: [],
         strep: []
     };
+    var gramstain = [];
     
     antibacData.forEach(function(d) {
         if (d != null) {
             var gramStain;
             // Converts gramstain string data to boolean
             if (d["Gram Staining "] == "positive") {
-                gramStain = true;
+                gramStain = "rgba(102, 51, 153, 0.7)";
             } else {
-                gramStain = false;
+                gramStain = "rgba(255, 47, 47, 0.7)";
             }
             
             plotData[d["Bacteria "]] = {
@@ -77,30 +112,49 @@ var formatData = function(antibacData) {
         antibiotics.neo.push(plotData[d].neomycin);
         antibiotics.peni.push(plotData[d].penicilin);
         antibiotics.strep.push(plotData[d].streptomycin);
+        gramstain.push(plotData[d].stain);
     });
-    console.log("neo data: ", antibiotics.neo);
-    console.log("peni data: ", antibiotics.peni);
-    console.log("strep data: ", antibiotics.strep);
+    
+    console.log("neo. data: ", antibiotics.neo);
+    console.log("peni. data: ", antibiotics.peni);
+    console.log("strep. data: ", antibiotics.strep);
+    
+    var stainConfig = {
+        color: gramstain,
+        width: 2
+    };
     
     var group1 = {
         x : keys,
         y : antibiotics.neo,
         name: 'Neomycin',
-        type: 'bar'
+        type: 'bar',
+        marker: {
+            color: 'rgba(220, 255, 209, 1)',
+            line: stainConfig
+        }
     };
     
     var group2 = {
         x : keys,
         y : antibiotics.peni,
         name: 'Penicilin',
-        type: 'bar'
+        type: 'bar',
+        marker: {
+            color: 'rgba(255, 209, 220, 1)',
+            line: stainConfig
+        }
     };
     
     var group3 = {
         x : keys,
         y: antibiotics.strep,
         name: 'Streptomycin',
-        type: 'bar'
+        type: 'bar',
+        marker: {
+            color: 'rgba(209, 220, 255, 1)',
+            line: stainConfig
+        }
     }
     
     var resultData = [group1, group2, group3] 
@@ -111,6 +165,80 @@ var formatData = function(antibacData) {
     return resultData;
 }
 
+var formatData2 = function(antibacData) {
+    console.log("Data formatting begins now ---------------");
+    var plotData = {};
+    var antibiotics = {
+        neomycin: [],
+        penicilin: [],
+        streptomycin: []
+    };
+    var gramstain = [];
+    
+    antibacData.forEach(function(d) {
+        if (d != null) {
+            var gramStain;
+            // Converts gramstain string data to boolean
+            if (d["Gram Staining "] == "positive") {
+                gramStain = "rgba(102, 51, 153, 0.7)";
+            } else {
+                gramStain = "rgba(255, 47, 47, 0.7)";
+            }
+            
+            plotData[d["Bacteria "]] = {
+                stain : gramStain,
+                neomycin : d["Neomycin"],
+                penicilin : d["Penicilin"],
+                streptomycin : d["Streptomycin "]
+            };
+        }
+    });
+    var keys = Object.keys(antibiotics);
+    var bactName = Object.keys(plotData);
+    console.log(keys);
+    console.log(bactName);
+    
+    antibacData.forEach(function(d) {
+        antibiotics.neomycin.push(d["Neomycin"]);
+        antibiotics.penicilin.push(d["Penicilin"]);
+        antibiotics.streptomycin.push(d["Streptomycin "]);
+    });
+    console.log(antibiotics);
+    
+    var name1 = new Array(16).fill('Neomycin');
+    var name2 = new Array(16).fill('Penicilin');
+    var name3 = new Array(16).fill('Streptomycin');
+    
+    var group1 = {
+        x: name1,
+        y: antibiotics.neomycin,
+        name: 'Neomycin',
+        type: 'box',
+        text: bactName
+    };
+
+    var group2 = {
+        x: name2,
+        y: antibiotics.penicilin,
+        name: 'Penicilin',
+        type: 'box',
+        text: bactName
+    };
+    
+    var group3 = {
+        x: name3,
+        y: antibiotics.streptomycin,
+        name: 'Streptomycin',
+        type: 'box',
+        text: bactName
+    };
+    
+    var resultData = [group1, group2, group3];
+    
+    console.log("Data formatting ends now -----------------");
+    
+    return resultData;
+}
 /*
  * Reads bacteria data from local file using ajax
  *
